685c448a8c25537acec0a60466b5db070021afd0
mmanikandap0512 <mmanikandap0512@gmail.com>
2025-08-12 16:00:18 +0530

    BMI Calculator

---
+++ BMICalculator/script.js
@@ -0,0 +1,18 @@
+const results = document.querySelector("#results");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector("#Height").value);
+  const weight = parseInt(document.querySelector("#Weight").value);
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please Enter valid Height ${height} is invalid`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please Enter valid Weight ${weight} is invalid`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
